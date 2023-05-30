ea1e17e41b7f622f0b5ac5d64fc86bf44b781ff5
Mateus <mateusstamm@alunos.utfp.edu.br>
2023-05-29 22:27:10 -0300

    Update insert_departamentos.dart

--- hasura_gamificacao/lib/departamentos/api/insert_departamentos.dart
+++ hasura_gamificacao/lib/departamentos/api/insert_departamentos.dart
@@ -4,7 +4,7 @@ import 'package:hasura_connect/hasura_connect.dart' hide Request, Response;
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 
-Future<Response> insertDepartamento(
+Future<Response> getAllDepartamentos(
   Request request,
   Injector injector,
   ModularArguments arguments,
@@ -19,6 +19,38 @@ Future<Response> insertDepartamento(
         }
       }
       ''');
+
+  return Response.ok(jsonEncode(hasuraResponse['data']));
+}
+
+Future<Response> insertDepartamento(
+  Request request,
+  Injector injector,
+  ModularArguments arguments,
+) async {
+  final hasuraConnect = injector.get<HasuraConnect>();
+
+  // Extract the parameters from the request URL
+  final Map<String, dynamic> params = request.url.queryParameters;
+
+  // Prepare the variables for the insert mutation
+  final variables = {
+    "nome": params[
+        'nome'], // Replace 'nome' with the actual parameter name for the department name
+  };
+
+  // Construct the mutation query
+  final mutation = '''
+    mutation InsertDepartamento(\$nome: String!) {
+      insert_departamentos(objects: {nome: \$nome}) {
+        affected_rows
+      }
+    }
+  ''';
+
+  // Execute the mutation
+  var hasuraResponse =
+      await hasuraConnect.mutation(mutation, variables: variables);
 
   return Response.ok(jsonEncode(hasuraResponse['data']));
 }
